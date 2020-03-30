7f879f26d42e0144a5955f5126cfbe7088bda043
Pharm_me <yuriy1635@gmail.com>
2020-03-30 20:29:01 +0300

    Change to Index2

--- src/project/urls.py
+++ src/project/urls.py
@@ -4,10 +4,15 @@ from django.http import HttpResponse
 from django.urls import path
 here = Path(__file__).parent.resolve()
 def view(r):
-    index = here.parent.parent / "index.html"
+    index = here.parent.parent / "index2.html"
     with index.open() as f:
         return HttpResponse(f.read())
+def viewstyle(r):
+    index = here.parent.parent / "style.css"
+    with index.open() as f:
+        return HttpResponse(f.read(), content_type="text/css")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', view),
+    path('css/', viewstyle),
 ]
